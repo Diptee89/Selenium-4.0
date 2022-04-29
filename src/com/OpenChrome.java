b3c4da2d48cae2954241f5386463920efe564337
DSingh <DSingh@INHYDLDSINGH2.logisticsdev.intra>
2022-04-29 13:01:36 +0530

    updated OpenChrome class

--- src/com/OpenChrome.java
+++ src/com/OpenChrome.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,7 +11,11 @@ public class OpenChrome {
 		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
 		
 		WebDriver driver= new ChromeDriver();
-		
+		driver.manage().window().maximize();
+
+		driver.navigate().to("https://google.com");
+		driver.navigate().back();
+		driver.navigate().forward();
 
 	}
 
